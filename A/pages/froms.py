66c1e55b8811acc8567d03c0728f024bc3d8c771
mojtaba <mojtaba@gmail.com>
2023-01-30 17:09:32 +0330

    create filter min max price and filter with brand and some fix bug in form search

--- A/pages/froms.py
+++ A/pages/froms.py
@@ -1,5 +1,24 @@
 from django import forms
+from products.models import Brand
+from django.forms import ModelForm, CheckboxInput
 
 
 class SearchForm(forms.Form):
     search = forms.CharField(label='')
+
+
+class FilterForm(forms.Form):
+    filter_min = forms.CharField()
+    filter_max = forms.CharField()
+
+
+# brand = Brand.objects.all()
+# print(brand)
+# for i in brand:
+#     print(i.slug)
+#
+#
+# class FilterBrand(forms.Form):
+#     for i in brand:
+#         i.slug = forms.CharField(widget=CheckboxInput())
+#     name = forms.CharField(widget=CheckboxInput())
--- A/pages/views.py
+++ A/pages/views.py
@@ -6,26 +6,56 @@ from .models import Category
 from products.models import Products
 from django.shortcuts import get_object_or_404
 from products.forms import CommendForm, CommendReplyForm
-from products.models import Commend
+from products.models import Commend, Brand
 from django.contrib.auth.decorators import login_required
-from .froms import SearchForm
+from .froms import SearchForm, FilterForm
 
 
 class HomeView(View):
     template_name = 'pages/index.html'
     form_class = SearchForm
+    form_class_2 = FilterForm
 
-    def get(self, request, slug_category=None):
+    def get(self, request, slug_category=None, brand_slug=None):
         categories = Category.objects.filter(is_sub_category=False)
         products = Products.objects.filter(is_active=True)
+        brand = Brand.objects.all()
+
+        if brand_slug:
+            # for filter product with brand
+            brand_s = Brand.objects.get(slug=brand_slug)
+            products = Products.objects.filter(is_active=True, brand=brand_s)
         if slug_category:
+            # for show information product
             category = Category.objects.filter(slug=slug_category)
             products = Products.objects.filter(Q(category__in=category))
         if request.GET.get('search'):
+            # search in product
             products = products.filter(title__contains=request.GET['search'])
 
+        if request.GET.get('filter_min') and request.GET.get('filter_max'):
+            # for filter product with min price and max price
+            products = products.filter(price__range=(request.GET.get('filter_min'), request.GET.get('filter_max')))
+        if request.GET.get('filter_min'):
+            # filter product with min price
+            filter = []
+            for item in products:
+                filter.append(item.price)
+            m = max(filter)
+            products = products.filter(price__range=(request.GET.get('filter_min'), m))
+        if request.GET.get('filter_max'):
+            # filter product with min price
+            filter = []
+            for item in products:
+                filter.append(item.price)
+            m = min(filter)
+            products = products.filter(price__range=(m, request.GET.get('filter_max')))
+        #     -------------
         return render(request, self.template_name,
-                      {'products': products, 'categories': categories, 'form': self.form_class})
+                      {'products': products, 'categories': categories, 'form_search': self.form_class,
+                       'filter': self.form_class_2, 'brand': brand})
+
+
 
 
 class ProductsDetailView(View):
