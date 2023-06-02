9aa5cbc5f8104a1cf5d52ee1cb223e8c6c876c8b
mojtaba <121169359+mojtabapaso@users.noreply.github.com>
2023-06-02 16:25:45 +0330

    Update settings.py
     change setting

--- A/A/settings.py
+++ A/A/settings.py
@@ -34,13 +34,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #  Add
     'django.contrib.humanize',
-
     # Local App
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'orders.apps.OrdersConfig',
     'products.apps.ProductsConfig',
-
     # Third Party Apps
     'jalali_date',
     'ckeditor',
@@ -56,12 +54,7 @@ JALALI_DATE_DEFAULTS = {
         'js': [
             # loading date picker
             'admin/js/django_jalali.min.js',
-            # OR
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
-            # 'admin/js/main.js',
+ 
         ],
         'css': {
             'all': [
